0306b898b54c25d181e103d41e9e6f2567caa78b
Jeyanth <jjeyanth2109@gmail.com>
2023-10-20 14:24:35 +0530

    initial commit

---
+++ MovieTicketBooking-jeyanth/MovieTicketBooking/Controllers/TicketController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MovieTicketBooking.Business.Service;
+using MovieTicketBooking.Data.Models.Dto;
+using System.Security.Claims;
+
+namespace MovieTicketBooking.Controllers
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize(Policy = "UserOnly")]
+    public class TicketController : ControllerBase
+    {
+        private readonly ITicketService _service;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="service"></param>
+        public TicketController(ITicketService service)
+        {
+            _service = service;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="model"></param>
+        /// <returns></returns>
+        [HttpPost]
+        [Route("{theatreId}/Book")]
+        public async Task<IActionResult> TicketBook([FromBody] TicketDto model, [FromRoute] string theatreId)
+        {
+            var userId = User.FindFirstValue("Id");
+            var response = await _service.TicketBook(model, userId, theatreId);
+
+            if (response.IsSuccess)
+            {
+                return Ok(response);
+            }
+            return BadRequest(response);
+        }
+    }
+}
---
+++ MovieTicketBooking-jeyanth/TestProject1/MovieControllerTests.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Moq;
+using MovieTicketBooking.Business.Service;
+using MovieTicketBooking.Controllers;
+using MovieTicketBooking.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieTicketBookingTests
+{
+    internal class MovieControllerTests
+    {
+        private Mock<IMovieService> _mockrepo;
+        private MovieController _controller;
+
+        [SetUp]
+        public void Initialize()
+        {
+            _mockrepo = new Mock<IMovieService>();
+            _controller = new MovieController(_mockrepo.Object);
+        }
+        [Test]
+        public async Task GetMovie()
+        {
+            Movie movie = new Movie();
+            string m;
+          _mockrepo.Setup(x => x.GetMovie(It.IsAny<string>())).ReturnsAsync(movie);
+          var Result= _controller.GetMovie();
+
+            Assert.IsNotNull(Result);
+        }      
+        
+    }
+}
